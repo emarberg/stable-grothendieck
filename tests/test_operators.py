0527ccf21e8f5883ce6fd2d697973d906627ff1e
Eric Marberg <eric.marberg@gmail.com>
2019-10-17 20:25:29 +0800

    experiments with shifted operators

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -1,18 +1,17 @@
 from vectors import Vector
-from partitions import Partition 
+from partitions import Partition
 
 
-def apply(*args):
-    assert len(args) > 0
-    if len(args) == 1:
-        return args[0] if type(args[0]) == Vector else Vector({args[0]: 1})
-    else:
-        vec = args[-1] if type(args[-1]) == Vector else Vector({args[-1]: 1})
-        op = args[-2]
-        ans = Vector(printer=vec.printer)
-        for mu in vec:
-            ans += vec[mu] * op(mu)
-        return apply(*(list(args)[:-2] + [ans]))
+def compose(*args):
+    def fn(x):
+        vec = x if type(x) == Vector else Vector({x: 1})
+        for op in reversed(args):
+            ans = Vector(printer=vec.printer)
+            for mu in vec:
+                ans += vec[mu] * op(mu)
+            vec = ans
+        return vec
+    return fn
 
 
 def u(i):
@@ -38,42 +37,447 @@ def d(i):
     return op
 
 
+def skew_symmetric_double(mu):
+    shape = {(a + 1, a + b + 1) for a in range(len(mu)) for b in range(1, mu[a] + 1)}
+    shape |= {(b, a) for (a, b) in shape}
+    a = 1
+    while True:
+        shape |= {(a, a)}
+        if (a, a + 1) not in shape:
+            break
+        a += 1
+    return shape
+
+
+def skew_symmetric_undouble(shape):
+    ans = []
+    for a, b in shape:
+        if a < b:
+            while a > len(ans):
+                ans += [0]
+            ans[a - 1] += 1
+    return tuple(ans)
+
+
+def symmetric_double(mu):
+    shape = {(a + 1, a + b) for a in range(len(mu)) for b in range(1, mu[a] + 1)}
+    shape |= {(b, a) for (a, b) in shape}
+    return shape
+
+
+def symmetric_undouble(shape):
+    ans = []
+    for a, b in shape:
+        if a <= b:
+            while a > len(ans):
+                ans += [0]
+            ans[a - 1] += 1
+    return tuple(ans)
+
+
+# def u_skew_symmetric(i):
+#     i, colummwise = abs(i), i > 0
+#     def op(mu):
+#         shape = skew_symmetric_double(mu)
+#         columns = {b for a, b in shape if a == i}
+#         b = (max(columns) + 1) if columns else 1
+#         if columnwise and i < b:
+#             return Vector()
+#         if i == 1 or (i - 1, b) in shape:
+#             shape |= {(i, b), (b, i)}
+#             return Vector({skew_symmetric_undouble(shape): 1})
+#         else:
+#             return Vector()
+#     return op
+
+
+# def d_skew_symmetric(i):
+#     i, colummwise = abs(i), i > 0
+#     def op(mu):
+#         shape = skew_symmetric_double(mu)
+#         columns = {b for a, b in shape if a == i and b != i}
+#         b = max(columns) if columns else 0
+#         if colummwise and i < b:
+#             return Vector()
+#         if b > 0 and (i + 1, b) not in shape:
+#             shape -= {(i, b), (b, i)}
+#             return Vector({skew_symmetric_undouble(shape): 1})
+#         else:
+#             return Vector()
+#     return op
+
+
+# def u_symmetric(i):
+#     i, colummwise = abs(i), i > 0
+#     def op(mu):
+#         shape = symmetric_double(mu)
+#         columns = {b for a, b in shape if a == i}
+#         b = (max(columns) + 1) if columns else 1
+#         if (colummwise and i < b) or (not colummwise and i > b):
+#             return Vector()
+#         if i == 1 or (i - 1, b) in shape:
+#             shape |= {(i, b), (b, i)}
+#             return Vector({symmetric_undouble(shape): 1})
+#         else:
+#             return Vector()
+#     return op
+
+
+# def d_symmetric(i):
+#     i, colummwise = abs(i), i > 0
+#     def op(mu):
+#         shape = symmetric_double(mu)
+#         columns = {b for a, b in shape if a == i}
+#         b = max(columns) if columns else 0
+#         if (colummwise and i < b) or (not colummwise and i > b):
+#             return Vector()
+#         if b > 0 and (i + 1, b) not in shape:
+#             shape -= {(i, b), (b, i)}
+#             return Vector({symmetric_undouble(shape): 1})
+#         else:
+#             return Vector()
+#     return op
+
+
+# def u_shifted(i, restricted_diagonal=False):
+#     def op(mu):
+#         if i == 0:
+#             return Vector({mu: 1})
+
+#         if i < 0:
+#             i = -i - 1
+#             if len(mu) < i or (0 < i < len(mu) and mu[i - 1] == mu[i] + 1) or (len(mu) == i and mu[-1] == 1):
+#                 return Vector()
+#             elif restricted_diagonal and i == len(mu):
+#                 return Vector()
+#             else:
+#                 mu = mu + (0,)
+#                 nu = mu[:i] + (mu[i] + 1,) + mu[i + 1:]
+#                 nu = nu[:-1] if nu[-1] == 0 else nu
+#                 return Vector({nu: 1})
+
+#         if i > 0:
+#             shape = {(a + 1, a + b) for a in range(len(mu)) for b in range(1, mu[a] + 1)}
+#             rows = {a for a, b in shape if i == b}
+#             a = (max(rows) + 1) if rows else 0
+#             if a == 0 or (a != i and (a, i - 1) not in shape):
+#                 return Vector()
+#             else:
+#                 mu = mu + (0,)
+#                 nu = mu[:a - 1] + (mu[a - 1] + 1,) + mu[a:]
+#                 nu = nu[:-1] if nu[-1] == 0 else nu
+#                 return Vector({nu: 1})
+#     return op
+
+
+# def d_shifted(i, restricted_diagonal=False):
+#     def op(mu):
+#         if i == 0:
+#             return Vector({mu: 1})
+
+#         if i < 0:
+#             i = -i - 1
+#             if len(mu) <= i or (i + 1 < len(mu) and mu[i + 1] == mu[i] - 1):
+#                 return Vector()
+#             else:
+#                 nu = mu[:i] + (mu[i] - 1,) + mu[i + 1:]
+#                 nu = nu[:-1] if nu[-1] == 0 else nu
+#                 return Vector({nu: 1})
+
+#         if i > 0:
+#             shape = {(a + 1, a + b) for a in range(len(mu)) for b in range(1, mu[a] + 1)}
+#             rows = {a for a, b in shape if i == b}
+#             a = (max(rows) - 1) if rows else -1
+#             if a == -1 or mu[a] + a != i:
+#                 return Vector()
+#             elif restricted_diagonal and a + 1 == len(mu) and mu[-1] == 1:
+#                 return Vector()
+#             else:
+#                 nu = mu[:a] + (mu[a] - 1,) + mu[i + 1:]
+#                 nu = nu[:-1] if nu[-1] == 0 else nu
+#                 return Vector({nu: 1})
+#     return op
+
+
+def u_symmetric(i):
+    def op(mu):
+        shape = {(a + 1, a + b) for a in range(len(mu)) for b in range(1, mu[a] + 1)}
+        a, b = 1, 1 + abs(i)
+        while (a, b) in shape:
+            a += 1
+            b += 1
+        if a > 1 and (a - 1, b) not in shape:
+            return Vector()
+        if a < b and (a, b - 1) not in shape:
+            return Vector()
+        shape |= {(a, b)}
+        return Vector({symmetric_undouble(shape): 1})
+    return op
+
+
+def d_symmetric(i):
+    def op(mu):
+        shape = {(a + 1, a + b) for a in range(len(mu)) for b in range(1, mu[a] + 1)}
+        cells = {(a, b) for (a, b) in shape if abs(a - b) == abs(i)}
+        if len(cells) == 0:
+            return Vector()
+        a, b = max(cells)
+        if (a + 1, b) in shape or (a, b + 1) in shape:
+            return Vector()
+        shape -= {(a, b)}
+        return Vector({symmetric_undouble(shape): 1})
+    return op
+
+
+
+
 def small_schur_expressions(n):
-    yield 'id', lambda x: x
-    # for i in range(1, n + 1):
-    #     yield 'u_%i' % i, u(i)
-    #     yield 'd_%i' % i, d(i)
+    yield (), compose()
+    for i in range(1, n + 1):
+        yield (i,), compose(u(i))
+        yield (-i,), compose(d(i))
 
     for i in range(1, n + 1):
         for j in range(1, n + 1):
-            if i != j:
-                yield 'u_%i u_%i' % (i, j), lambda x: apply(u(i), u(j), x)
-                yield 'd_%i d_%i' % (i, j), lambda x: apply(d(i), d(j), x)
-            yield 'u_%i d_%i' % (i, j), lambda x: apply(u(i), d(j), x)
-            yield 'd_%i u_%i' % (i, j), lambda x: apply(d(i), u(j), x)
+            yield (i, j), compose(u(i), u(j))
+            yield (-i, -j), compose(d(i), d(j))
+            yield (i, -j), compose(u(i), d(j))
+            yield (-i, j), compose(d(i), u(j))
 
-    # for i in range(1, n + 1):
-    #     for j in range(1, n + 1):
-    #         for k in range(1, n + 1):
-    #             yield (i, j, k), lambda x: u(i)(u(j)(u(k)(x)))
-    #             yield (i, j, k), lambda x: d(i)(d(j)(d(k)(x)))
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            for k in range(1, n + 1):
+                yield (i, j, k), compose(u(i), u(j), u(k))
+                yield (-i, -j, -k), compose(d(i), d(j), d(k))
 
 
-def test_find_relations(n=5):
+def span(relations, tup):
+    ans = set()
+    add = {tup}
+    while add:
+        ans |= add
+        new_add = set()
+
+        for tup in add:
+            for m in range(1, len(tup) + 1):
+                for i in range(len(tup) - m + 1):
+                    a, b, c = tup[:i], tup[i:i + m], tup[i + m:]
+                    x = relations.get(b, b)
+                    if x != b and len(x) <= len(b):
+                        new_add.add(a + x + c)
+
+        add = new_add - ans
+    return ans
+
+
+def get_dict(expected):
+    dictionary = {}
+    for pair in expected:
+        a, b = tuple(pair)
+        dictionary[a] = b
+        dictionary[b] = a
+    return dictionary
+
+
+def test_schur_relations(n=5):
+
+    def tag(args):
+        if len(args) == 0:
+            return 'id'
+        s = ''
+        for a in args:
+            if a > 0:
+                s += 'u_%i ' % a
+            else:
+                s += 'd_%i ' % -a
+        return s.strip()
+
     square = (n + 1) * (n + 1,)
     partitions = list(Partition.subpartitions(square))
     small = {k: v for k, v in small_schur_expressions(n)}
+
+    def equals(k1, k2):
+        op1, op2 = small[k1], small[k2]
+        return all(op1(mu) == op2(mu) for mu in partitions)
+
+    expected = [
+        {(), (-1, 1)},
+    ] + [
+        {(i, j), (j, i)} for i in range(1, n + 1) for j in range(1, n + 1) if i + 1 < j
+    ] + [
+         {(-i, -j), (-j, -i)} for i in range(1, n + 1) for j in range(1, n + 1) if i + 1 < j
+    ] + [
+        {(-i, j), (j, -i)} for i in range(1, n + 1) for j in range(1, n + 1) if i != j
+    ] + [
+        {(-i - 1, i + 1), (i, -i)} for i in range(1, n)
+    ] + [
+        {(i + 1, i, i), (i, i + 1, i)} for i in range(1, n)
+    ] + [
+        {(i + 1, i, i + 1), (i + 1, i + 1, i)} for i in range(1, n)
+    ] + [
+        {(-i, -i - 1, -i), (-i, -i, -i - 1)} for i in range(1, n)
+    ] + [
+        {(-i, -i - 1, -i - 1), (-i - 1, -i, -i - 1)} for i in range(1, n)
+    ]
+    get_dict(expected)
+
+    def check_expected_identities(k1, k2):
+        if {k1, k2} in expected:
+            try:
+                assert equals(k1, k2)
+            except:
+                print()
+                print('** failure: ', tag(k1), '!=', tag(k2))
+                raise Exception
+            return True
+        return False
+
+    relations = set()
     for k1 in small:
-        print()
         for k2 in small:
-            if k1 != k2:
-                op1 = small[k1]
-                op2 = small[k2]
-                if all(op1(mu) == op2(mu) for mu in partitions) and not all(op1(mu).is_zero() for mu in partitions):
-                    print('\n\n\n\n')
-                    for mu in partitions:
-                        print(k1, mu, '=', op1(mu), '==', op2(mu), '=', k2, mu, '??', apply(d(2), d(1), mu))
-                        print()
-                    print('*', k1, '=', k2)
+            if k1 < k2:
+                if check_expected_identities(k1, k2):
+                    relations.add((k1, k2))
+                    print()
+                    print('* expected: ', tag(k1), '=', tag(k2))
+                    continue
+                if span(dictionary, k1) & span(dictionary, k2):
+                    continue
+                if equals(k1, k2):
+                    relations.add((k1, k2))
+                    print()
+                    print('unexpected:', tag(k1), '=', tag(k2))
+                    raise Exception
     print()
+    return relations
+
+
+def small_q_schur_expressions(n):
+    return small_shifted_schur_expressions(n, u_symmetric, d_symmetric)
+
+
+def small_p_schur_expressions(n):
+    return small_shifted_schur_expressions(n, u_skew_symmetric, d_skew_symmetric)
+
+
+def small_shifted_schur_expressions(n, f, g):
+    yield (), compose()
+
+    for i in range(0, n + 1):
+        if i == 0:
+            continue
+        yield (('u', i),), compose(f(i))
+        yield (('d', i),), compose(g(i))
+
+    for i in range(0, n + 1):
+        for j in range(0, n + 1):
+            if i == 0 or j == 0:
+                continue
+            yield (('u', i), ('u', j)), compose(f(i), f(j))
+            yield (('d', i), ('d', j)), compose(g(i), g(j))
+            yield (('u', i), ('d', j)), compose(f(i), g(j))
+            yield (('d', i), ('u', j)), compose(g(i), f(j))
+
+    for i in range(0, n + 1):
+        for j in range(0, n + 1):
+            for k in range(0, n + 1):
+                if i == 0 or j == 0 or k == 0:
+                    continue
+                yield (('u', i), ('u', j), ('u', k)), compose(f(i), f(j), f(k))
+                yield (('d', i), ('d', j), ('d', k)), compose(g(i), g(j), g(k))
+
+
+def test_q_schur_relations(n=5):
+
+    def tag(args):
+        if len(args) == 0:
+            return 'id'
+        s = ''
+        for a in args:
+            s += '%s_{%i} ' % a
+        return s.strip()
+
+    delta = tuple(range(n + 1, 0, -1))
+    partitions = list(Partition.subpartitions(delta, strict=True))
+    small = {k: v for k, v in small_q_schur_expressions(n)}
+
+    def equals(k1, k2):
+        op1, op2 = small[k1], small[k2]
+        return all(op1(mu) == op2(mu) for mu in partitions)
+
+    expected = []
+    # [
+    #     {(), (('d', -1), ('u', -1))},
+    #     #
+    #     {(('u', -1), ('d', 1)), (('u', 1), ('d', 1))},
+    #     {(('u', 1), ('d', -1)), (('u', 1), ('d', 1))},
+    #     #
+    #     {(('d', -1), ('u', 1)), (('d', 1), ('u', 1))},
+    #     {(('d', 1), ('u', -1)), (('d', 1), ('u', 1))},
+    #     #
+    #     {(('u', -1), ('u', 1)), (('u', 2), ('u', 1))},
+    #     {(('d', 1), ('d', -1)), (('d', 1), ('d', 2))}
+    # ] + [
+    #     {(('u', i), ('u', j)), (('u', j), ('u', i))} for i in range(1, n + 1) for j in range(1, n + 1) if i + 1 < j
+    # ] + [
+    #     {(('u', -i), ('u', -j)), (('u', -j), ('u', -i))} for i in range(1, n + 1) for j in range(1, n + 1) if i + 1 < j
+    # ] + [
+    #     {(('d', i), ('d', j)), (('d', j), ('d', i))} for i in range(1, n + 1) for j in range(1, n + 1) if i + 1 < j
+    # ] + [
+    #     {(('d', -i), ('d', -j)), (('d', -j), ('d', -i))} for i in range(1, n + 1) for j in range(1, n + 1) if i + 1 < j
+    # ] + [
+    #     {(('d', -i), ('u', i)), (('d', i), ('u', -i))} for i in range(1, n + 1)
+    # ] + [
+    #     {(('u', -i), ('d', i)), (('u', i), ('d', -i))} for i in range(1, n + 1)
+    # ] + [
+    #     {(('d', i), ('u', j)), (('u', j), ('d', i))} for i in range(1, n + 1) for j in range(1, n + 1) if i != j
+    # ] + [
+    #     {(('d', -i), ('u', -j)), (('u', -j), ('d', -i))} for i in range(1, n + 1) for j in range(1, n + 1) if i != j
+    # ] + [
+    #     {(('u', i), ('u', i + 1), ('u', i)), (('u', i + 1), ('u', i), ('u', i))} for i in range(1, n)
+    # ] + [
+    #     {(('u', -i - 1), ('u', -i - 1), ('u', -i)), (('u', -i - 1), ('u', -i), ('u', -i - 1))} for i in range(1, n)
+    # ]
+    dictionary = get_dict(expected)
+
+    def check_expected_identities(k1, k2):
+        if {k1, k2} in expected:
+            try:
+                assert equals(k1, k2)
+            except:
+                print()
+                print('** failure: ', tag(k1), '!=', tag(k2))
+                # for mu in partitions:
+                #     op1 = small[k1]
+                #     op2 = small[k2]
+                #     if op1(mu) != op2(mu):
+                #         print('  ', mu, ':', op1(mu), '!=', op2(mu))
+                # print()
+                #raise Exception
+            return True
+        return False
+
+    relations = set()
+    for k1 in sorted(small):
+        #if all(small[k1](mu).is_zero() for mu in partitions):
+            #print('***** zero:', tag(k1))
+            #continue
+        for k2 in sorted(small):
+            if k1 < k2:
+                if check_expected_identities(k1, k2):
+                    relations.add((k1, k2))
+                    # print()
+                    # print('* expected: ', tag(k1), '=', tag(k2))
+                    continue
+                if all(small[k1](mu).is_zero() for mu in partitions):
+                    continue
+                if span(dictionary, k1) & span(dictionary, k2):
+                    continue
+                if equals(k1, k2):
+                    relations.add((k1, k2))
+                    print()
+                    print('unexpected:', tag(k1), '=', tag(k2))
+                    # raise Exception
+    print()
+    return relations
 
